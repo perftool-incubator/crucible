45f0b785a3df37ce6562e72cc289f7c8c4b69b57
Andrew Theurer <atheurer@redhat.com>
2020-11-05 21:25:11 -0500

    -No need to chown /etc/elasticsearch

--- config/start-es.sh
+++ config/start-es.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 es_dir="$1"
-chown -R elasticsearch:elasticsearch $es_dir /etc/elasticsearch
+chown -R elasticsearch:elasticsearch $es_dir
 mkdir -p /var/lib/crucible/logs
 su -c "/usr/share/elasticsearch/bin/elasticsearch -Epath.data=$es_dir" elasticsearch 2>&1 | tee /var/lib/crucible/logs/elastic.log
