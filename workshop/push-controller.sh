af1a642758251d0419927ca137a9ad4af5ea7438
Karl Rister <krister@redhat.com>
2023-10-25 09:02:37 -0500

    refactor push-controller.sh
    - use controller.conf to share information with build-controller.sh
    
    - use a combination of the date, git commit, and system architecture
      to create a image tag

--- workshop/push-controller.sh
+++ workshop/push-controller.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
-tag="${1}"
+if [ ! -e /etc/sysconfig/crucible ]; then
+    echo "ERROR: /etc/sysconfig/crucible does not exist"
+    exit 1
+else
+    source /etc/sysconfig/crucible
+fi
 
-if [ -z "${tag}" ]; then
-    echo "ERROR: You must supply a tag"
+if [ -z "${CRUCIBLE_HOME}" ]; then
+    echo "ERROR: \$CRUCIBLE_HOME not defined"
     exit 1
 fi
 
-source="localhost/workshop/fedora36_crucible-controller"
-destination="quay.io/crucible/controller:${tag}"
+if pushd ${CRUCIBLE_HOME} > /dev/null; then
+    THE_DATE=$(date +%Y-%m-%d)
+    THE_ARCH=$(uname -m)
+    THE_COMMIT=$(git rev-parse HEAD)
 
-cmd="buildah push ${source} ${destination}"
+    TAG="${THE_DATE}_${THE_COMMIT}_${THE_ARCH}"
 
-echo "Running: ${cmd}"
-${cmd}
+    if [ -z "${THE_DATE}" -o -z "${THE_ARCH}" -o -z "${THE_COMMIT}" ]; then
+	echo "ERROR: Could not determine proper tag [${TAG}]"
+	exit 1
+    fi
+
+    source ./workshop/controller.conf
+
+    source="localhost/workshop/${controller_userenv}_crucible-controller"
+    destination="quay.io/crucible/controller:${TAG}"
+
+    cmd="buildah push ${source} ${destination}"
+
+    echo "Running: ${cmd}"
+    ${cmd}
+else
+    echo "ERROR: Failed to pushd to \$CRUCIBLE_HOME [${CRUCIBLE_HOME}]"
+    exit 1
+fi
